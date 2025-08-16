efa9364170dd1a4bdbd2584ca9d392ac3cf1691f
EunBae Gong <geb9211@gmail.com>
2025-08-16 20:00:01 +0900

    chore: pretendard 폰트 세팅

--- apps/teampage/tailwind.config.ts
+++ apps/teampage/tailwind.config.ts
@@ -1,15 +1,20 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+			},
+			fontFamily: {
+				bold: ["Pretendard-Bold", "sans-serif"],
+				semibold: ["Pretendard-SemiBold", "sans-serif"],
+				medium: ["Pretendard-Medium", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
