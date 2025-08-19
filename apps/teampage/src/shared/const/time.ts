8e5f29c28f53dd7b93106dffd0ed79dbfb33bf8b
jongbin26 <010jjj@naver.com>
2025-08-19 23:00:20 +0900

    feat: 시간 관련 상수 정의

---
+++ apps/teampage/src/shared/const/time.ts
@@ -0,0 +1,15 @@
+const MS = 1000;
+const SEC = 1;
+const MIN = SEC * 60;
+const HOUR = MIN * 60;
+const DAY = HOUR * 24;
+const WEEK = DAY * 7;
+
+export const TIME = {
+  MS,
+  SEC,
+  MIN,
+  HOUR,
+  DAY,
+  WEEK,
+};
