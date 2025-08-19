dbd6d06d40d2da86a7ca9ae3be2c8e3cfe2d16c2
jongbin26 <010jjj@naver.com>
2025-08-19 23:46:52 +0900

    feat: 변경된 테이블에 맞춰 매니저 및 클라이언트 함수 수정

--- apps/teampage/src/shared/notion/NotionManager.ts
+++ apps/teampage/src/shared/notion/NotionManager.ts
@@ -1,14 +1,14 @@
 import { SingletonRegister, Singletons } from "@shared/utils/SingletonRegistry";
 import { NotionClient } from "./NotionClient";
 import { NotionUtil } from "./NotionUtil";
-import type { PeopleData, NotionDatabaseResponse } from "./NotionType";
+import type { PeopleData, NotionListResponse } from "./NotionType";
 
-const TOKEN = "NOTION_MANAGER";
+const TOKEN = "NOTION_MANAGER" as const;
 @SingletonRegister(TOKEN)
 export class NotionManager {
   public static TOKEN = TOKEN;
 
-  private getNotionDatabase = async (): Promise<NotionDatabaseResponse> => {
+  private getNotionList = async (): Promise<NotionListResponse> => {
     const notion = Singletons[NotionClient.TOKEN].getInstance();
     if (!process.env.NOTION_PEOPLE_DATABASE_ID) {
       throw new Error("NOTION_PEOPLE_DATABASE_ID is not set");
@@ -18,21 +18,20 @@ export class NotionManager {
       database_id: process.env.NOTION_PEOPLE_DATABASE_ID,
     });
 
-    return NotionUtil.parseNotionDatabase(response);
+    return NotionUtil.parseNotionList(response);
   };
 
   public getPeopleData = async (): Promise<PeopleData[]> => {
     try {
-      const database = await this.getNotionDatabase();
-
-      const peopleData: PeopleData[] = database.results.map((page) => {
+      const list = await this.getNotionList();
+      const peopleData: PeopleData[] = list.results.map((page) => {
         return NotionUtil.convertToPeopleData(page);
       });
 
       return peopleData;
     } catch (error) {
-      console.error("Failed to fetch or parse people data:", error);
-      throw new Error("Failed to get people data from Notion");
+      console.error("Failed to get people data from Notion", error);
+      return [];
     }
   };
 }
