ecdca7f1497327b208b14c36e0a1ed73f36dc048
jongbin26 <010jjj@naver.com>
2025-08-30 11:48:55 +0900

    feat: Header 스타일 수정 및 auth 연결

---
+++ apps/teampage/src/shared/auth/useAuth.ts
@@ -0,0 +1,34 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { Session } from 'next-auth';
+
+type SessionType = {
+  accessToken: string;
+  refreshToken: string;
+} & Session;
+
+export const useAuth = () => {
+  const [session, setSession] = useState<SessionType | null>(null);
+  const { data: rawSession, status } = useSession();
+
+  const safeParseSession = (session: Session): session is SessionType => {
+    if ('accessToken' in session && 'refreshToken' in session) {
+      return true;
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    if (rawSession && safeParseSession(rawSession)) {
+      setSession(rawSession);
+    }
+  }, [rawSession]);
+
+  return {
+    session,
+    status,
+    signIn,
+    signOut,
+  };
+};
