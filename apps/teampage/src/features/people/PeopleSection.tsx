cc507c3ff35930075b202915ab1e40f3eae56a4f
jongbin26 <010jjj@naver.com>
2025-08-31 21:53:39 +0900

    feat: 검색어 기반으로 구성원 렌더링되도록 구현

--- apps/teampage/src/features/people/PeopleSection.tsx
+++ apps/teampage/src/features/people/PeopleSection.tsx
@@ -1,39 +1,45 @@
 'use client';
-import Tab from './Tab';
 import PeopleCard from './PeopleCard';
 import { useContext } from 'react';
 import { PeopleContext, PeopleProvider } from './PeopleProvider';
 import { PeopleData } from '@features/notion/NotionType';
+import { SearchField } from '@/shared/component/search-field';
 
 const PeopleSectionContent = ({ peopleData }: { peopleData: PeopleData[] }) => {
-  const { selectedGeneration, setSelectedGeneration } =
+  const { selectedGeneration, debouncedSearchQuery } =
     useContext(PeopleContext)!;
 
   const sortGenerationWithNumber = (a: string, b: string) => {
     return Number(a.slice(0, -1)) - Number(b.slice(0, -1));
   };
 
-  const generations = Array.from(
-    new Set(peopleData.map((person) => person.generation)),
-  ).sort(sortGenerationWithNumber);
+  const generations = [
+    '전체 기수',
+    ...Array.from(new Set(peopleData.map((person) => person.generation))).sort(
+      sortGenerationWithNumber,
+    ),
+  ];
 
   return (
-    <main className="flex flex-col items-center justify-center py-[100px]">
-      <Tab
-        tabs={generations}
-        selectedIdx={selectedGeneration}
-        onChange={(idx) => {
-          setSelectedGeneration(idx);
-        }}
-      />
-      <div className="flex gap-5">
-        {peopleData
-          .filter(
-            (person) => person.generation === generations[selectedGeneration],
-          )
-          .map((person, index) => (
-            <PeopleCard key={`${person.name}-${index}`} person={person} />
-          ))}
+    <main className="flex items-center justify-center py-[100px]">
+      <div className="w-[1120px] flex flex-col gap-10">
+        <PeopleHeader />
+        <div className="flex gap-5">
+          {peopleData
+            .filter(
+              (person) =>
+                selectedGeneration === -1 ||
+                person.generation === generations[selectedGeneration],
+            )
+            .filter((person) =>
+              person.name
+                .toLowerCase()
+                .includes(debouncedSearchQuery.toLowerCase()),
+            )
+            .map((person, index) => (
+              <PeopleCard key={`${person.name}-${index}`} person={person} />
+            ))}
+        </div>
       </div>
     </main>
   );
@@ -48,3 +54,18 @@ const PeopleSection = ({ peopleData }: { peopleData: PeopleData[] }) => {
 };
 
 export default PeopleSection;
+
+const PeopleHeader = () => {
+  const { searchQuery, setSearchQuery } = useContext(PeopleContext)!;
+  return (
+    <div className="w-full flex justify-between items-center">
+      <SearchField
+        size="small"
+        placeholder="제목을 입력해주세요"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        onClear={() => setSearchQuery('')}
+      />
+    </div>
+  );
+};
