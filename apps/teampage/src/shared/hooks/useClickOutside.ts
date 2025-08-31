5819d169f5d5b42900945f66473097423f804b32
jongbin26 <010jjj@naver.com>
2025-08-31 22:59:46 +0900

    feat: useClickOutside 구현

---
+++ apps/teampage/src/shared/hooks/useClickOutside.ts
@@ -0,0 +1,26 @@
+import { useEffect, useCallback, useRef } from 'react';
+
+export const useClickOutside = <T extends HTMLElement>({
+  callback,
+}: {
+  callback: () => void;
+}): { ref: React.RefObject<T> } => {
+  const ref = useRef<T>(null);
+  const handleClick = useCallback(
+    (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node & T)) {
+        callback();
+      }
+    },
+    [ref, callback],
+  );
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, [handleClick]);
+
+  return { ref };
+};
