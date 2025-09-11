d6e2bf248a30a119e3a5be7b83fdfd63225c056d
EunBae Gong <geb9211@gmail.com>
2025-09-11 17:07:24 +0900

    feat: 메인페이지 section03 구현

--- apps/teampage/src/page/main-page/section/Section03.tsx
+++ apps/teampage/src/page/main-page/section/Section03.tsx
@@ -1,97 +1,142 @@
 import { Text } from '@shared/component/Text';
-import { Divider } from '@/shared/component/Divider';
+import Image from 'next/image';
 
-export function Section01() {
+export function Section03() {
   return (
-    <div className="flex flex-col items-center justify-center gap-20 my-20">
-      <div className="flex flex-col items-center text-center gap-6">
-        <Text variant="heading-220-b" color="primary-gradiant">
-          TEAM UOSLIFE
-        </Text>
-        <Text
-          variant="title-60-b"
-          className="whitespace-pre-line text-5xl text-gray-800"
-        >
-          {'시대생 팀은 IT 기술을 통해\n편리하고 즐거운 학교생활을 만듭니다.'}
-        </Text>
-      </div>
-      <div className="flex flex-col items-center gap-20 max-w-pc w-full">
-        <div className="flex flex-col items-center text-center gap-4">
-          <div className="text-4xl font-bold">
-            <Text variant="title-40-b" color="primary-ui">
-              About Our Story
-            </Text>
-          </div>
-          <div className="text-gray-500 text-xl">
-            <Text variant="title-28-b" className="text-grey-600">
-              <span>시대생은 </span>
-              <span className="text-grey-900">서울시립대학교의 </span>
-              <span className="font-bold text-gray-900">중앙 IT동아리</span>
-              <span>입니다. 시대생 팀은</span>
-              <br />
-              <span className="text-gray-800">
-                시대생 앱을 포함한 다양한 IT 서비스
-              </span>
-              <span>를 만들고 운영하고 있습니다.</span>
-            </Text>
-          </div>
+    <div className="relative flex flex-col items-center justify-center gap-40 my-32 pb-[520px]">
+      <div className="flex flex-col items-center text-center gap-4">
+        <div className="text-4xl font-bold">
+          <Text variant="title-28-b" color="grey-500">
+            How We Make It Real
+          </Text>
         </div>
-        <div className="flex w-full gap-6">
-          {STORY_BOX_DATA.map((data) => (
-            <OurStoryBox
-              key={data.title}
-              title={data.title}
-              description={data.description}
-              content={data.content}
+        <div className="text-gray-500 text-xl">
+          <Text as="h2" variant="title-60-b">
+            <span>시대생은 </span>
+            <Text as="span" variant="title-60-b" color="primary-gradiant">
+              PM · 개발 · 디자인 · 마케팅{' '}
+            </Text>
+            <span>챕터로 조직되어</span>
+            <br />
+            <span>아이디어를 실제 서비스로 구현합니다.</span>
+          </Text>
+        </div>
+      </div>
+      <div className="relative flex flex-row justify-between gap-20 pb-[320px]">
+        <div className="flex flex-col gap-4 mt-[54px]">
+          <Text variant="title-28-b" color="primary-ui">
+            시간표 · 공지사항
+          </Text>
+          <Text
+            variant="title-48-b"
+            className="whitespace-pre-line"
+          >{`학교 생활에 꼭\n필요한\n유틸리티부터`}</Text>
+        </div>
+        <div className="flex flex-row gap-4">
+          <Image
+            src={'/img/section03_mock_01.webp'}
+            alt={''}
+            width={340}
+            height={740}
+            className="pb-32"
+          />
+          <Image
+            src={'/img/section03_mock_02.webp'}
+            alt={''}
+            width={340}
+            height={740}
+            className="pt-32"
+          />
+        </div>
+
+        <div className="absolute bottom-0 flex flex-row-reverse items-end justify-between gap-20">
+          <div className="flex flex-col gap-4 mt-[54px] pb-32">
+            <Text variant="title-28-b" color="primary-ui">
+              시대팅
+            </Text>
+            <Text
+              variant="title-48-b"
+              className="whitespace-pre-line"
+            >{`대학생활을 더 풍요롭게\n만들어주는 매칭 서비스까지`}</Text>
+          </div>
+          <Image
+            src={'/img/section03_mock_03.webp'}
+            alt={''}
+            width={340}
+            height={740}
+          />
+        </div>
+      </div>
+      <div className="absolute bottom-0 left-0 w-full px-8 rounded-[62px] h-[600px] flex flex-col items-center justify-end gap-8 pb-12">
+        <Image
+          src="/img/section03_bg.webp"
+          alt="section 03 background"
+          fill
+          className="absolute -z-10 top-0 left-0 w-full"
+        />
+        <div className="flex flex-col gap-5 items-center">
+          <Image
+            src="/img/uoslife_logo_white.png"
+            alt="uoslife_logo_white"
+            height={72}
+            width={72}
+            className="rounded-2xl"
+          />
+          <Text
+            variant="title-40-b"
+            color="white"
+            className="whitespace-pre-line text-center"
+          >{`새로운 연결을 확장하는\n서비스를 운영하고 있습니다.`}</Text>
+        </div>
+        <div className="flex flex-row gap-3">
+          <a
+            href="https://apps.apple.com/kr/app/%EC%8B%9C%EB%8C%80%EC%83%9D-%EB%82%B4-%EC%86%90%EC%95%88%EC%9D%98-%EC%84%9C%EC%9A%B8%EC%8B%9C%EB%A6%BD%EB%8C%80%ED%95%99%EA%B5%90/id1514073192"
+            target="_blank"
+            rel="noopener"
+            className="flex flex-row gap-2 items-center justify-center rounded-xl px-4 py-2 bg-grey-800"
+          >
+            <Image
+              src="/svg/ios_logo.svg"
+              alt="ios logo"
+              height={19}
+              width={22}
             />
-          ))}
+            <Text
+              variant="body-14-m"
+              color="white"
+              className="flex items-center gap-1"
+            >
+              <Text as="span" variant="body-16-b" color="white">
+                iOS 앱
+              </Text>
+              다운로드
+            </Text>
+          </a>
+          <a
+            href="https://play.google.com/store/apps/details?id=com.beyondconnect.uoslife&hl=ko"
+            target="_blank"
+            rel="noopener"
+            className="flex flex-row gap-2 items-center justify-center rounded-xl px-4 py-2 bg-grey-800"
+          >
+            <Image
+              src="/svg/google_play_logo.svg"
+              alt="google play logo"
+              height={19}
+              width={22}
+            />
+            <Text
+              variant="body-14-m"
+              color="white"
+              className="flex items-center gap-1"
+            >
+              <Text as="span" variant="body-16-b" color="white">
+                Android 앱
+              </Text>
+              다운로드
+            </Text>
+          </a>
         </div>
       </div>
-    </div>
-  );
-}
-
-const STORY_BOX_DATA = [
-  {
-    title: 'Since',
-    description: '시작 연도',
-    content: '2020~',
-  },
-  {
-    title: 'Alumni',
-    description: '졸업 기수',
-    content: '100+',
-  },
-  {
-    title: 'Active Members',
-    description: '활동 멤버',
-    content: '25+',
-  },
-];
-
-function OurStoryBox({
-  title,
-  description,
-  content,
-}: {
-  title: string;
-  description: string;
-  content: string;
-}) {
-  return (
-    <div className="w-full flex flex-col items-center justify-center gap-6 p-5 h-[300px] bg-gray-100 rounded-3xl">
-      <div className="flex flex-col items-center text-center">
-        <Text variant="title-28-b" color="grey-700">
-          {title}
-        </Text>
-        <Text variant="body-18-m" color="grey-600">
-          {description}
-        </Text>
-      </div>
-      <Divider />
-      <Text variant="heading-80-b" color="primary-gradiant">
-        {content}
-      </Text>
     </div>
   );
 }
