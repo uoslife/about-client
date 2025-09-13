d35e1622d0a69fa3fdb869fca0c92f0ce65e66f3
jongbin26 <010jjj@naver.com>
2025-09-13 15:54:44 +0900

    feat: ClientProvider에 AmplitudeProvider 추가

--- apps/teampage/src/app/provider/client-provider.tsx
+++ apps/teampage/src/app/provider/client-provider.tsx
@@ -1,10 +1,11 @@
-"use client";
-import { SessionProvider } from "next-auth/react";
+'use client';
+import AnalyticsContextProvider from '@/entities/analytics/useAnalytics';
+import { SessionProvider } from 'next-auth/react';
 
 export const ClientProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <SessionProvider refetchInterval={5 * 60} refetchOnWindowFocus={true}>
-      {children}
+      <AnalyticsContextProvider>{children}</AnalyticsContextProvider>
     </SessionProvider>
   );
 };
