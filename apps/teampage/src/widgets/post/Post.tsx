316d239291e20fcfdb6286132c482b6e4ec565d6
jongbin26 <010jjj@naver.com>
2025-09-21 17:09:07 +0900

    feat: view_article amplitude 연결

--- apps/teampage/src/widgets/post/Post.tsx
+++ apps/teampage/src/widgets/post/Post.tsx
@@ -1,12 +1,12 @@
+'use client';
 import { PostSection } from '@/features/post/PostSection';
 import { PostType } from '@/features/post/PostHeader';
+import { useSendViewAmplitudeEvent } from '@/entities/analytics/useSendViewAmplitudeEvent';
 
-export async function Post({
-  id,
-  category,
-}: {
-  id: number;
-  category: PostType;
-}) {
+export function Post({ id, category }: { id: number; category: PostType }) {
+  useSendViewAmplitudeEvent('VIEW_ARTICLE', {
+    tab_name: category,
+    article_id: id.toString(),
+  });
   return <PostSection type={category} id={id} />;
 }
