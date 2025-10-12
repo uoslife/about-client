70e2ba0e5ea49458bfce7767f1ec90d9066c902b
jongbin26 <010jjj@naver.com>
2025-10-12 21:02:28 +0900

    feat: 변경된 text 디자인 시스템 반영

--- apps/teampage/src/shared/component/Text.tsx
+++ apps/teampage/src/shared/component/Text.tsx
@@ -1,12 +1,9 @@
 import type React from 'react';
 
 export type TextVariant =
-  | 'heading-220-b'
   | 'heading-100-b'
-  | 'heading-80-b'
-  | 'heading-76-b'
   | 'heading-72-b'
-  | 'title-60-b'
+  | 'heading-60-b'
   | 'title-48-b'
   | 'title-40-b'
   | 'title-40-m'
@@ -15,14 +12,22 @@ export type TextVariant =
   | 'title-28-m'
   | 'title-24-b'
   | 'title-24-m'
+  | 'title-20-b'
+  | 'title-20-m'
+  | 'title-18-b'
+  | 'title-18-m'
+  | 'title-16-b'
+  | 'title-16-m'
   | 'body-20-b'
   | 'body-20-m'
   | 'body-18-b'
   | 'body-18-m'
   | 'body-16-b'
   | 'body-16-m'
-  | 'body-14-sb'
-  | 'body-14-m';
+  | 'body-14-b'
+  | 'body-14-m'
+  | 'body-12-b'
+  | 'body-12-m';
 
 export type ColorValue =
   | 'grey-900'
@@ -50,28 +55,33 @@ interface TextProps extends React.HTMLAttributes<HTMLParagraphElement> {
 }
 
 const variantToClassMap: Record<TextVariant, string> = {
-  'heading-220-b': 'text-heading-220-b font-bold',
-  'heading-100-b': 'text-heading-100-b font-bold',
-  'heading-80-b': 'text-heading-80-b font-bold',
-  'heading-76-b': 'text-heading-76-b font-bold',
-  'heading-72-b': 'text-heading-72-b font-bold',
-  'title-60-b': 'text-title-60-b font-bold',
-  'title-48-b': 'text-title-48-b font-bold',
-  'title-40-b': 'text-title-40-b font-bold',
-  'title-40-m': 'text-title-40-m font-medium',
-  'title-36-b': 'text-title-36-b font-bold',
-  'title-28-b': 'text-title-28-b font-bold',
-  'title-28-m': 'text-title-28-m font-medium',
-  'title-24-b': 'text-title-24-b font-bold',
-  'title-24-m': 'text-title-24-m font-medium',
-  'body-20-b': 'text-body-20-b font-bold',
-  'body-20-m': 'text-body-20-m font-medium',
-  'body-18-b': 'text-body-18-b font-bold',
-  'body-18-m': 'text-body-18-m font-medium',
-  'body-16-b': 'text-body-16-b font-bold',
-  'body-16-m': 'text-body-16-m font-medium',
-  'body-14-sb': 'text-body-14-sb font-semibold',
-  'body-14-m': 'text-body-14-m font-medium',
+  'heading-100-b': 'text-[100px] leading-[1.2] font-bold',
+  'heading-72-b': 'text-[72px] leading-[1.2] font-bold',
+  'heading-60-b': 'text-[60px] leading-[1.4] font-bold',
+  'title-48-b': 'text-[48px] leading-[1.4] font-bold',
+  'title-40-b': 'text-[40px] leading-[1.4] font-bold',
+  'title-40-m': 'text-[40px] leading-[1.4] font-semibold',
+  'title-36-b': 'text-[36px] leading-[1.4] font-bold',
+  'title-28-b': 'text-[28px] leading-[1.5] font-bold',
+  'title-28-m': 'text-[28px] leading-[1.5] font-medium',
+  'title-24-b': 'text-[24px] leading-[1.5] font-bold',
+  'title-24-m': 'text-[24px] leading-[1.5] font-medium',
+  'title-20-b': 'text-[20px] leading-[1.6] font-bold',
+  'title-20-m': 'text-[20px] leading-[1.6] font-medium',
+  'title-18-b': 'text-[18px] leading-[1.6] font-bold',
+  'title-18-m': 'text-[18px] leading-[1.6] font-medium',
+  'title-16-b': 'text-[16px] leading-[1.6] font-bold',
+  'title-16-m': 'text-[16px] leading-[1.6] font-medium',
+  'body-20-b': 'text-[20px] leading-[1.6] font-bold',
+  'body-20-m': 'text-[20px] leading-[1.6] font-medium',
+  'body-18-b': 'text-[18px] leading-[1.6] font-bold',
+  'body-18-m': 'text-[18px] leading-[1.6] font-medium',
+  'body-16-b': 'text-[16px] leading-[1.6] font-bold',
+  'body-16-m': 'text-[16px] leading-[1.6] font-medium',
+  'body-14-b': 'text-[14px] leading-[1.6] font-bold',
+  'body-14-m': 'text-[14px] leading-[1.6] font-medium',
+  'body-12-b': 'text-[12px] leading-[1.5] font-bold',
+  'body-12-m': 'text-[12px] leading-[1.5] font-medium',
 };
 
 const colorToClassMap: Record<ColorValue, string> = {
