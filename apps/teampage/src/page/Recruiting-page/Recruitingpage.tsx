342f5a76e1357a7bf75b545c340b4cf75b8aa204
root <root@kmj.localdomain>
2025-11-18 22:15:25 +0900

    chore: 주석추가, 페이지 테스트 버전으로 변경

--- apps/teampage/src/page/Recruiting-page/Recruitingpage.tsx
+++ apps/teampage/src/page/Recruiting-page/Recruitingpage.tsx
@@ -14,16 +14,20 @@ export default function RecruitingPage() {
   const startDate = new Date('2024-11-24T00:00:00');
   const endDate = new Date('2024-11-30T23:59:59');
   const isInRange = now >= startDate && now <= endDate;
-  const ref2 = useRef<HTMLDivElement>(null);
+  const ref5 = useRef<HTMLDivElement>(null);
   const [showButton, setShowButton] = useState(true);
 
-  useEffect(() => {
+  // 이벤트 리스너로 현재 스크롤의 위치를 파악해서 
+  // ref5에 저장된 위치와 비교함.
+  // 오프셋을 42.5*8 만큼 줘서(버튼이 떠있는 위치, 수정 할수도)
+  // 섹션6에 버튼이 닿았을 때 발동하게 함 
+  useEffect(() => { 
     const handleScroll = () => {
-      if (!ref2.current) {
+      if (!ref5.current) {
         return;
       }
 
-      const section06Top = ref2.current.getBoundingClientRect().top;
+      const section06Top = ref5.current.getBoundingClientRect().top;
       const windowHeight = window.innerHeight;
 
       if (section06Top + 42.5 * 8 <= windowHeight) {
@@ -43,18 +47,16 @@ export default function RecruitingPage() {
   return (
     <div className="flex flex-col  gap-40 items-center w-full bg-[#FFF]">
       <main className="w-full">
-        
+        {showButton && <Recruitbutton className={`fixed top-3/4 left-1/2 -translate-x-1/2 -translate-y-3/4`} />}
+        {/* 버튼 isInRange && 붙여서 랜더링 안되게 */}
 
-        {isInRange &&showButton && <Recruitbutton className={`fixed top-3/4 left-1/2 -translate-x-1/2 -translate-y-3/4`} />}
-
-        <section id="">
-          <Section01 />
-          <Section02 />
-          <Section03 />
-          <Section04 />
-          <Section05 />
-        </section>
-        <Section06 ref={ref2} />
+        <Section01 />
+        <Section02 />
+        <Section03 />
+        <Section04 />
+        <Section05 />
+        {/* 나중에 1~5는 기간 검사해서 랜더링 안되게 */}
+        <Section06 ref={ref5} />
         <Section07 />
       </main>
     </div>
