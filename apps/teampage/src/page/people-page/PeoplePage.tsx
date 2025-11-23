bedd295ad6f881ecfc47a700b8544c556f125257
jongbin26 <010jjj@naver.com>
2025-11-24 00:41:23 +0900

    chore: people page revalidate 50분으로 설정

--- apps/teampage/src/page/people-page/PeoplePage.tsx
+++ apps/teampage/src/page/people-page/PeoplePage.tsx
@@ -2,7 +2,7 @@ import { Singletons } from '@shared/utils/SingletonRegistry';
 import { NotionManager } from '@features/notion';
 import PeopleSection from '@/features/people/PeopleSection';
 
-export const revalidate = false;
+export const revalidate = 3000;
 
 export default async function PeoplePage() {
   const notionManager = Singletons[NotionManager.TOKEN];
