7e20879e86426c3965609f3b4c6620004de2e53b
goeun kim <rlarhdms6828@naver.com>
2026-01-07 02:10:05 +0900

    chore: our story hover container position 수정

--- apps/teampage/src/widgets/header/Header.tsx
+++ apps/teampage/src/widgets/header/Header.tsx
@@ -256,7 +256,7 @@ export default function Header() {
 
 function OurStoryMenu() {
   return (
-    <div className="hidden group-hover:block top-full w-40 bg-white box-border flex-col gap-2 items-start justify-start p-[12px] rounded-2xl shadow-[0px_0px_12px_0px_rgba(18,18,18,0.1)] static">
+    <div className="hidden group-hover:block top-full w-40 bg-white box-border flex-col gap-2 items-start justify-start p-[12px] rounded-2xl shadow-[0px_0px_12px_0px_rgba(18,18,18,0.1)] absolute transform translate-x-[-22%]">
       <Link href="/career" className="w-full group/career">
         <div className="box-border content-stretch flex flex-row gap-2.5 h-11 items-center justify-center px-4 py-1.5 relative rounded-[40px] w-full hover:bg-gray-100">
           <Text variant="body-18-m" className="group-hover/career:text-primary-ui">
